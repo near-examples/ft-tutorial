18e2d72ed7d95a64c1eac40e1b47e1485f8d64d8
BenKurrek <bkurrek@uwaterloo.ca>
2022-08-16 12:35:22 -0400

    added skeleton contract

---
+++ skeleton/src/metadata.rs
@@ -0,0 +1,30 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::Base64VecU8;
+use near_sdk::serde::{Deserialize, Serialize};
+use near_sdk::{ext_contract, near_bindgen};
+
+use crate::*;
+
+#[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct FungibleTokenMetadata {
+    /*
+        FILL THIS IN
+    */
+}
+
+#[ext_contract(ext_ft_metadata)]
+pub trait FungibleTokenMetadataProvider {
+    // View call for returning the contract metadata
+    fn ft_metadata(&self) -> FungibleTokenMetadata;
+}
+
+#[near_bindgen]
+impl FungibleTokenMetadataProvider for Contract {
+    fn ft_metadata(&self) -> FungibleTokenMetadata {
+        /*
+            FILL THIS IN
+        */
+        todo!(); //remove once code is filled in.
+    }
+}
