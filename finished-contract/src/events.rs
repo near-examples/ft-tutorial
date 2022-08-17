931904f439e982e44578a5e44e713ada7716aebd
BenKurrek <bkurrek@uwaterloo.ca>
2022-08-17 09:40:40 -0400

    removed burn event

--- finished-contract/src/events.rs
+++ finished-contract/src/events.rs
@@ -96,30 +96,6 @@ impl FtTransfer<'_> {
     }
 }
 
-/// Data to log for an FT burn event. To log this event, call [`.emit()`](FtBurn::emit).
-#[must_use]
-#[derive(Serialize, Debug, Clone)]
-pub struct FtBurn<'a> {
-    pub owner_id: &'a AccountId,
-    pub amount: &'a U128,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub memo: Option<&'a str>,
-}
-
-impl FtBurn<'_> {
-    /// Logs the event to the host. This is required to ensure that the event is triggered
-    /// and to consume the event.
-    pub fn emit(self) {
-        Self::emit_many(&[self])
-    }
-
-    /// Emits an FT burn event, through [`env::log_str`](near_sdk::env::log_str),
-    /// where each [`FtBurn`] represents the data of each burn.
-    pub fn emit_many<'a>(data: &'a [FtBurn<'a>]) {
-        new_141_v1(Nep141EventKind::FtBurn(data)).emit()
-    }
-}
-
 #[derive(Serialize, Debug)]
 pub(crate) struct Nep141Event<'a> {
     version: &'static str,
@@ -134,7 +110,6 @@ pub(crate) struct Nep141Event<'a> {
 enum Nep141EventKind<'a> {
     FtMint(&'a [FtMint<'a>]),
     FtTransfer(&'a [FtTransfer<'a>]),
-    FtBurn(&'a [FtBurn<'a>]),
 }
 
 fn new_141<'a>(version: &'static str, event_kind: Nep141EventKind<'a>) -> NearEvent<'a> {
